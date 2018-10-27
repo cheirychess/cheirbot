7110269811028ccb616b42c6a90806de95353c6b
cheirychess <32835844+cheirychess@users.noreply.github.com>
2018-10-26 20:13:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -57,6 +57,48 @@ client.on('message', message => {
        message.reply('https://cdn.discordapp.com/attachments/369575741639229460/447859322022658081/0cc.png');
        
     }
+});       
+    
+client.on('message', message => {
+   if (message.content === 'fat demo') {
+       message.reply('https://cdn.discordapp.com/attachments/467857158625689610/505152121436504075/meme.png');
+       
+    }
+});       
+    
+client.on('message', message => {
+   if (message.content === 'can u aint') {
+       message.reply('https://media.discordapp.net/attachments/254248946086576128/498969306839777280/image0.jpg');
+       
+    }
+});
+
+client.on('message', message => {
+   if (message.content === 'voring distance') {
+       message.reply('https://cdn.discordapp.com/attachments/467858965141913603/496404778012901388/gplus300042337.png');
+       
+    }
+});
+
+client.on('message', message => {
+   if (message.content === 'Okay, this is epic') {
+       message.reply('https://cdn.discordapp.com/attachments/467858965141913603/495840798747656192/image0.png');
+       
+    }
+});
+
+client.on('message', message => {
+   if (message.content === 'my fat fucking nuts') {
+       message.reply('https://i.ytimg.com/vi/WZ6Z_ATIcRw/maxresdefault.jpg');
+       
+    }
+});
+
+client.on('message', message => {
+   if (message.content === 'me after spotting a minority') {
+       message.reply('https://cdn.discordapp.com/attachments/467857158625689610/473674660551196672/unknown.png');
+       
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
